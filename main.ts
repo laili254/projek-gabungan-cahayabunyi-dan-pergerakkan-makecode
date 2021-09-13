78d8796088b1945c4acf8a3461d3c21f19d3e5a4
laili254 <90621678+laili254@users.noreply.github.com>
2021-09-13 20:32:41 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    servos.P0.setAngle(90)
+    music.playTone(262, music.beat(BeatFraction.Breve) * 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    servos.P0.setAngle(0)
+})
+pins.digitalWritePin(DigitalPin.P1, 1)
